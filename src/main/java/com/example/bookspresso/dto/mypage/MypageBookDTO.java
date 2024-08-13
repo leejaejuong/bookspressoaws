4d63a1052221662c8017ae2826ccafba7702345d
shindongeok <ehrflqakstp@gmail.com>
2024-08-13 18:36:30 +0900

    ãmyDebpageDebate무리 , mypageBook 마ã수정

--- src/main/java/com/example/bookspresso/dto/mypage/MypageBookDTO.java
+++ src/main/java/com/example/bookspresso/dto/mypage/MypageBookDTO.java
@@ -5,7 +5,8 @@ import lombok.*;
 @Getter @Setter @ToString
 @NoArgsConstructor @AllArgsConstructor @Builder
 public class MypageBookDTO {
-    String isbn13;
-    String title;
-    String cover;
+    private Long meetingId;
+    private Long debateId; // 토론 id
+    private String title; // 책 제목
+    private String cover; // 책 표지
 }
